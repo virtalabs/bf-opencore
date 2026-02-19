9d14af8695ab678d88927327c74a2d1e91ad17f1
Taylor Cochran <tcochran.dev@gmail.com>
2026-02-19 13:39:56 -0800

    Make fingerprint an integration

--- bf_opencore/exceptions.py
+++ bf_opencore/exceptions.py
@@ -13,45 +13,28 @@ class AIMSConnectorError(BlueFlowError):
     """Base exception for AIMS connector errors."""
 
 
-class ConnectorTaskError(BlueFlowError):
-    """Thrown by connector tasks."""
+class IntegrationTaskError(BlueFlowError):
+    """Thrown by integration tasks."""
 
-
-class TMSAssetError(TMSConnectorError, ConnectorTaskError):
-    """Thrown by the TMS connector.
-
-    Circumstances include: TMS server-side failure when connecting to the TMS
-    API, malformed data received from TMS, misconfiguration such as incorrect
-    credentials.
-    """
-
-
-class AIMSAssetError(AIMSConnectorError, ConnectorTaskError):
-    """Thrown by the AIMS connector.
-
-    Circumstances include: AIMS server-side failure when connecting to the AIMS
-    API, malformed data received from AIMS, misconfiguration such as incorrect
-    credentials.
-    """
 
 
 class MalformedFileError(BlueFlowError, ValueError):
     """Thrown when importing a malformed file."""
 
 
-class ConnectorConfigError(BlueFlowError):
+class IntegrationConfigError(BlueFlowError):
     """Thrown by connector users.
 
     This happens if we try to use an insufficiently configured connector.
     """
 
 
-class ConnectorRemoteError(BlueFlowError):
-    """Thrown by connector users.
+class IntegrationRemoteError(BlueFlowError):
+    """Thrown by integration users.
 
     This happens if we're not successful in connecting to remote server.
     """
 
 
-class ConnectorOSError(BlueFlowError):
-    """Thrown if connector causes OSError."""
+class IntegrationOSError(BlueFlowError):
+    """Thrown if integration causes OSError."""
