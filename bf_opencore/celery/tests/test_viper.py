9f6bd5a0faf36a548c15240a5b1ff132d2278646
Taylor Cochran <tcochran.dev@gmail.com>
2026-02-20 12:45:12 -0800

    Setup test for task

---
+++ bf_opencore/celery/tests/test_viper.py
@@ -0,0 +1,28 @@
+from unittest.mock import patch
+from bf_opencore.models.viper import ViperWebhookRequest, ViperWebhookResponse, ViperAsset
+from bf_opencore.celery.tasks import viper_webhook
+
+def test_viper_webhook_output(celery_app):
+    '''
+    Captures the output from the celery task.
+    Ensuring it's the same as the expected output.
+    '''
+    with patch('requests.post') as mock_post:
+        viper_webhook.delay(ViperWebhookRequest(
+            callback='https://example.com/viper/webhook/',
+            since='2026-01-01T00:00:00Z',
+            before='2026-01-02T00:00:00Z',
+            page=1,
+            page_size=10,
+        ))
+        assert mock_post.call_count == 1
+        assert mock_post.call_args[0][0] == 'https://example.com/viper/webhook/'
+        assert mock_post.call_args[0][1] == ViperWebhookResponse(
+            items=[],
+            page=1,
+            page_size=10,
+            total=0,
+            total_pages=0,
+            next_page=None,
+            previous_page=None,
+        ).to_dict()
