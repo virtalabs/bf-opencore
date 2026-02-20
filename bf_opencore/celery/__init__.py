a5d8e74ddfb5815b0f13d600ab8d7bd1090f8805
Taylor Cochran <tcochran.dev@gmail.com>
2026-02-20 12:40:36 -0800

    Cleanup imports

--- bf_opencore/celery/__init__.py
+++ bf_opencore/celery/__init__.py
@@ -18,12 +18,6 @@ $ python
 """
 import celery
 import logging
-import requests
-from django.apps import apps
-from bf_opencore.models import Asset
-import json
-import math
-from bf_opencore.models.viper import ViperWebhookRequest, ViperWebhookResponse, ViperAsset
 
 # register signals
 from bf_opencore import signals
@@ -48,28 +42,3 @@ celery_app.config_from_object(
     silent=False,
 )
 
-
-@celery_app.task
-def viper_webhook(viper_data: ViperWebhookRequest):
-    """Process a viper webhook."""
-    logger.info(f"Processing viper webhook: {viper_data}")
-    Asset = apps.get_model('bf_opencore', 'Asset')
-    assets = Asset.objects.filter(last_pinged__gte=viper_data.since)
-    if viper_data.before:
-        assets = assets.filter(last_pinged__lte=viper_data.before)
-    assets = assets.order_by('last_pinged').all()
-    total = assets.count()
-    total_pages = math.ceil(total / viper_data.page_size)
-    for i in range(0, len(assets), viper_data.page_size):
-        assets_chunk = assets[i:i + viper_data.page_size]
-        viper_assets = [ViperAsset(asset) for asset in assets_chunk]
-        viper_response = ViperWebhookResponse(
-            items=viper_assets,
-            page=viper_data.page,
-            page_size=viper_data.page_size,
-            total=total,
-            total_pages=total_pages,
-            next_page=None,
-            previous_page=None
-        )
-        requests.post(viper_data.callback, json.dumps(viper_response))
