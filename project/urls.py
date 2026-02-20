df9c613fda83fe57ef7bb897a93ec7bfe6e8d821
Taylor Cochran <tcochran.dev@gmail.com>
2026-02-20 10:31:50 -0800

    Register admin url

--- project/urls.py
+++ project/urls.py
@@ -2,8 +2,10 @@
 
 """Root URL configuration. Includes bf_opencore app URLs."""
 
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("api/", include("bf_opencore.urls")),
 ]
