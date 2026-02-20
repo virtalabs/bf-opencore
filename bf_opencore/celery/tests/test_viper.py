c3079e7f0e6dbd4f5872f5036515ca429de4cdd0
Taylor Cochran <tcochran.dev@gmail.com>
2026-02-20 13:25:27 -0800

    Fix task calls

--- bf_opencore/celery/tests/test_viper.py
+++ bf_opencore/celery/tests/test_viper.py
@@ -1,30 +1,55 @@
 from unittest.mock import patch
 from bf_opencore.models.viper import ViperWebhookRequest, ViperWebhookResponse, ViperAsset
 from bf_opencore.celery.tasks import viper_webhook
+from django.apps import apps
 import pytest
+import math
+import json
+
+def get_asset_count():
+    Asset = apps.get_model('bf_opencore', 'Asset')
+    return Asset.objects.count()
 
 @pytest.mark.django_db
-def test_viper_webhook_output(celery_app):
+def test_viper_webhook_output_no_assets(celery_app):
     '''
     Captures the output from the celery task.
     Ensuring it's the same as the expected output.
     '''
     with patch('bf_opencore.celery.tasks.requests.post') as mock_post:
-        viper_webhook.delay(ViperWebhookRequest(
+        viper_webhook.apply(args=[ViperWebhookRequest(
             callback='https://example.com/viper/webhook/',
             since='2026-01-01T00:00:00Z',
             before='2026-01-02T00:00:00Z',
             page=1,
             page_size=10,
-        ).to_dict())
-        assert mock_post.call_count == 1
-        assert mock_post.call_args[0][0] == 'https://example.com/viper/webhook/'
-        assert mock_post.call_args[0][1] == ViperWebhookResponse(
-            items=[],
+        ).to_dict()])
+        assert mock_post.call_count == 0
+
+@pytest.mark.django_db
+def test_viper_webhook_output_with_assets(celery_app, setup_assets):
+    '''
+    Captures the output from the celery task.
+    Ensuring it's the same as the expected output.
+    '''
+    page_size = 10
+    total_assets = get_asset_count()
+    total_pages = math.ceil(total_assets / page_size)
+    with patch('bf_opencore.celery.tasks.requests.post') as mock_post:
+        viper_webhook.apply(args=[ViperWebhookRequest(
+            callback='https://example.com/viper/webhook/',
+            since='1800-01-01T00:00:00Z', # some arbitrary date in the past to get all assets
+            before=None,
             page=1,
-            page_size=10,
-            total=0,
-            total_pages=0,
-            next_page=None,
-            previous_page=None,
-        ).to_dict()
+            page_size=page_size,
+        ).to_dict()])
+        assert mock_post.call_count == total_pages
+        assert mock_post.call_args[0][0] == 'https://example.com/viper/webhook/'
+        for i, call in enumerate(mock_post.call_args_list):
+            payload = call.args[1]
+            assert payload['page'] == 1 + i
+            assert payload['page_size'] == page_size
+            assert payload['total'] == total_assets
+            assert payload['total_pages'] == total_pages
+            assert payload['next_page'] is None
+            assert payload['previous_page'] is None
