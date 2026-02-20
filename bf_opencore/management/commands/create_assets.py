07fec1ebcfd1fed2cbf60a8622752d82cd3c8a7a
Taylor Cochran <tcochran.dev@gmail.com>
2026-02-20 11:12:45 -0800

    Add in asset data dump

---
+++ bf_opencore/management/commands/create_assets.py
@@ -0,0 +1,57 @@
+"""Load assets from a json file."""
+
+import json
+from django.core.management.base import BaseCommand
+from django.apps import apps
+from typing import Generator
+
+
+def make_assets(data: dict) -> Generator[dict, None, None]:
+    for id, asset in data.items():
+        yield {
+            'id': id,
+            'name': asset['name'],
+            'hostname': asset['hostname'],
+            'ip_address': asset['ip_address'],
+            'mac_address': asset['mac_address'],
+            'nic_vendor': asset['nic_vendor'],
+            'manufacturer': asset['manufacturer'],
+            'model': asset['model'],
+            'serial_number': asset['serial_number'],
+            'udi': asset['udi'],
+            "hostname": asset['hostname'],
+            "ip_address": asset['ip_address'],
+            "mac_address": asset['mac_address'],
+            "nic_vendor": asset['nic_vendor'],
+            "manufacturer": asset['manufacturer'],
+            "model": asset['model'],
+            "serial_number": asset['serial_number'],
+            "udi": asset['udi'],
+            "tag_number": asset['tag_number'],
+            "category": asset['category'],
+            "date_added": asset['date_added'],
+            "owner": asset['owner'],
+            "os": asset['os'],
+            "app_sw_version": asset['app_sw_version'],
+            "last_scanned": asset['last_scanned'],
+            "last_pinged": asset['last_pinged'],
+            "open_ports_tcp": asset['open_ports_tcp'],
+            "external_keys": asset['external_keys'],
+        }
+
+class Command(BaseCommand): 
+    """Django manage.py sub command loads assets from a json file."""
+
+    help = 'Load assets from a json file'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--filepath', type=str, required=True, help='The path to the json file')
+
+    def handle(self, *args, **options) -> None:
+        Asset = apps.get_model('bf_opencore', 'Asset')
+        file_path = options.get('filepath')
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.loads(file.read())
+            assets = make_assets(data)
+            Asset.objects.bulk_create([Asset(**asset) for asset in assets])
+        self.stdout.write(self.style.SUCCESS(f'Loaded {len(Asset.objects.all())} assets'))
