bb2bb69abed6667d1eb3c7b7159768bb636e80cd
Taylor Cochran <tcochran.dev@gmail.com>
2026-02-26 10:34:33 -0800

    Add in custom base Task
    Task accounts for failures and logs them.
    Also defines some basic retry logic

--- bf_opencore/celery/tasks.py
+++ bf_opencore/celery/tasks.py
@@ -1,4 +1,5 @@
 from bf_opencore.celery import celery_app
+from celery import Task as BaseTask
 from bf_opencore.models.viper import ViperWebhookRequest, ViperWebhookResponseList
 
 import logging
@@ -6,7 +7,18 @@ import requests
 
 logger = logging.getLogger(__name__)
 
-@celery_app.task
+class Task(BaseTask):
+    # https://docs.celeryq.dev/en/main/userguide/tasks.html#Task.autoretry_for
+    max_retries = 5
+    retry_backoff = True
+    retry_backoff_max = 10 # seconds
+    retry_jitter = True
+    dont_auto_retry_for = (TypeError,)
+
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        logger.error(f"[!!] {task_id} failed: {exc}")
+
+@celery_app.task(base=Task)
 def viper_webhook(data: dict):
     """Process a viper webhook."""
     viper_data = ViperWebhookRequest(**data)
@@ -15,3 +27,4 @@ def viper_webhook(data: dict):
     for response in response_list:
         as_dict = response.to_dict()
         requests.post(viper_data.callback, json=as_dict, headers={'Content-Type': 'application/json'})
+
