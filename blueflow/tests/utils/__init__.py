6710c9287d3104c08618d6e1cb1cb2c2337fa1ca
Taylor Cochran <tcochran.dev@gmail.com>
2026-05-05 12:01:38 -0700

    chore: migrate os.path / os.remove / open() to pathlib (PTH)
    Automated commit by quil coder agent.

--- blueflow/tests/utils/__init__.py
+++ blueflow/tests/utils/__init__.py
@@ -1,6 +1,7 @@
 """test utilities."""
 
 import os
+import tempfile
 from pathlib import Path
 
 
@@ -14,21 +15,13 @@ def path_nparent(path, n):
 BLUEFLOW_HOME = path_nparent(__file__, 5)
 
 
-def write_tempfile(text, bom_utf8=False):
+def write_tempfile(text, *, bom_utf8=False):
     """Write text (ostensibly, CSV) to a temp file and return the filename."""
     csvfd, filename = tempfile.mkstemp(suffix=".csv")
-    if bom_utf8:
-        # The 'utf-8-sig' special encoding inserts the byte order mark
-        # '0xef, 0xbb, 0xdf' at the beginning of the file (or strips it
-        # off, if it's there when reading.)
-        # NOTE: in general it is *discouraged* to use this BOM.  We use
-        # it here only to test that we're robust against it if it is in
-        # a file we come across.
-        #
-        # https://docs.python.org/3/library/codecs.html#encodings-and-unicode
-        encoding = "utf-8-sig"
-    else:
-        encoding = None
+    # 'utf-8-sig' inserts/strips the BOM (0xef 0xbb 0xbf); discouraged in
+    # general but tested here to verify robustness against files that carry it.
+    # https://docs.python.org/3/library/codecs.html#encodings-and-unicode
+    encoding = "utf-8-sig" if bom_utf8 else None
     with os.fdopen(csvfd, "w", encoding=encoding) as fh:
         fh.write(text)
     return filename
