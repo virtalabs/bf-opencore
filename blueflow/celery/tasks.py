7c76853dbe91fd8840bda2f6d587450d4950ec2a
Taylor Cochran <tcochran.dev@gmail.com>
2026-05-27 17:08:34 -0500

    Use the reponse.json

--- blueflow/celery/tasks.py
+++ blueflow/celery/tasks.py
@@ -45,7 +45,7 @@ def _send_viper_payload(viper_data: ViperWebhookRequest, request_id: str) -> str
         if response.status_code >= 400:
             return json.dumps(response.json, indent=4)
         response.raise_for_status()
-        viper_responses.append(response)
+        viper_responses.append(response.json)
     return json.dumps(viper_responses, indent=4)
 
 
