4d93354f060845fdc614146cc8d963574a3b688f
Taylor Cochran <tcochran.dev@gmail.com>
2026-06-11 11:49:42 -0700

    Strip out unused network endpoint code
    Tons more no longer referd

--- blueflow/models/network_endpoint.py
+++ blueflow/models/network_endpoint.py
@@ -13,58 +13,6 @@ from blueflow.utils import DisableSignals
 class NetworkEndpointManager(models.Manager):
     """Manager maintains priority."""
 
-    def normalize_and_get(self, **kwargs):
-        """Return one canonical NetworkEndpoint, deleting ambigious matches."""
-        fields_by_priority = ["mac_address", "ipv4_address", "ipv6_address"]
-        f = models.Q()
-        for field in fields_by_priority:
-            if kwargs.get(field):
-                f |= models.Q(**{field: kwargs[field]})
-
-        results = super().filter(f)
-
-        if results.count() == 0:
-            NetworkEndpoint = apps.get_model("blueflow", "NetworkEndpoint")
-            raise NetworkEndpoint.DoesNotExist()
-        if results.count() == 1:
-            return results.first()
-        # if there is more than one match, mac_address must be set
-        chosen = results.get(mac_address=kwargs["mac_address"])
-        others = results.exclude(pk=chosen.pk)
-        for endpoint in others:
-            success = chosen.merge(endpoint)
-            if success:
-                # merged the details we care about
-                endpoint.delete()
-            else:
-                # IP conflicts exist, clear them
-                if endpoint.ipv4_address == kwargs.get("ipv4_address"):
-                    endpoint.ipv4_address = None
-                if endpoint.ipv6_address == kwargs.get("ipv6_address"):
-                    endpoint.ipv6_address = None
-                endpoint.save()
-            chosen.save()
-        return chosen
-
-    def update_or_create_by_priority(self, defaults=None, **kwargs):
-        """Return or create a NetworkEndpoint, prefering Mac address."""
-        if not defaults:
-            defaults = {}
-        NetworkEndpoint = apps.get_model("blueflow", "NetworkEndpoint")
-        try:
-            match = self.normalize_and_get(**kwargs)
-            for key, value in defaults.items():
-                setattr(match, key, value)
-            created = False
-        except NetworkEndpoint.DoesNotExist:
-            new_values = kwargs
-            new_values.update(defaults)
-            match = NetworkEndpoint(**new_values)
-            created = True
-
-        match.save()
-        return (match, created)
-
 
 class EndpointSuggestion(models.Model):
     """An inferred connection between an Asset and a NetworkEndpoint."""
@@ -75,15 +23,12 @@ class EndpointSuggestion(models.Model):
     confidence_limit = models.SmallIntegerField(default=0)
     evidence = models.JSONField(default=list)
 
+    def __str__(self) -> str:
+        return super().__str__()
+
 
 class NetworkEndpoint(models.Model):
     """Holds an device observed on the network."""
-
-    class Meta:
-        unique_together = (
-            ("mac_address", "ipv4_address"),
-            ("mac_address", "ipv6_address"),
-        )
 
     mac_address = MACAddressField(unique=True, null=True, blank=True)
     ipv4_address = InetAddressField(
@@ -122,6 +67,27 @@ class NetworkEndpoint(models.Model):
 
     # override objects to use our custom manager
     objects = NetworkEndpointManager()
+
+    class Meta:
+        unique_together = (
+            ("mac_address", "ipv4_address"),
+            ("mac_address", "ipv6_address"),
+        )
+
+    def __hash__(self):
+        """Hash this model."""
+        # combination of Model type and PK is unique
+        return hash((self.__class__, self.pk))
+
+    def __eq__(self, other):
+        """Equal if both NetworkEndpoint and primary key is equal."""
+        return isinstance(other, self.__class__) and self.pk == other.pk
+
+    def __str__(self):
+        return (
+            f"Netflow endpoint with MAC {self.mac_address} "
+            f"and IP {(self.ipv4_address, self.ipv6_address)}"
+        )
 
     def add_transmit_ports(self, ports):
         """Set transmit_ports to transmit_ports union ports."""
@@ -199,104 +165,6 @@ class NetworkEndpoint(models.Model):
         ).delete()
         self.update_suggestions()
 
-    def compare(self, asset):
-        """Return a report on likelihood of this endpoint being the asset."""
-        confidence = 0
-        evidence = []
-
-        matching_criteria = [
-            {
-                "reason": "Devices on same network",
-                "value": lambda asset: (
-                    self.ipv4_address
-                    and asset.network_qset()
-                    .filter(cidr__cidr__net_contains=self.ipv4_address)
-                    .first()
-                    .name
-                ),
-                "weight": lambda asset: 1,
-                "max_weight": 1,
-                "test": lambda asset: (
-                    self.ipv4_address
-                    and asset.network_qset()
-                    .filter(cidr__cidr__net_contains=self.ipv4_address)
-                    .exists()
-                ),
-            },
-            {
-                "reason": "Devices on same network",
-                "value": lambda asset: (
-                    self.ipv6_address
-                    and asset.network_qset()
-                    .filter(cidr__cidr__net_contains=self.ipv6_address)
-                    .first()
-                    .name
-                ),
-                "weight": lambda asset: 1,
-                "max_weight": 1,
-                "test": lambda asset: (
-                    self.ipv6_address
-                    and asset.network_qset()
-                    .filter(cidr__cidr__net_contains=self.ipv6_address)
-                    .exists()
-                ),
-            },
-            {
-                "reason": "Shared MAC address range",
-                "value": lambda asset: f"{str(self.mac_address)[:9]}*",
-                "weight": lambda asset: 4,
-                "max_weight": 4,
-                "test": lambda asset: (
-                    self.mac_address
-                    and asset.mac_address
-                    and self.mac_address.value >> 24 == asset.mac_address.value >> 24
-                ),
-            },
-            {
-                "reason": "Shared TCP ports",
-                "value": lambda asset: {
-                    int(port.strip("/TCP"))
-                    for port in self.receive_ports
-                    if port.endswith("/TCP")
-                }.intersection(set(asset.open_ports_tcp)),
-                "weight": lambda asset: min(
-                    len(
-                        {
-                            int(port.strip("/TCP"))
-                            for port in self.receive_ports
-                            if port.endswith("/TCP")
-                        }.intersection(set(asset.open_ports_tcp))
-                    ),
-                    3,
-                ),
-                "max_weight": 3,
-                "test": lambda asset: (
-                    not {
-                        int(port.strip("/TCP"))
-                        for port in self.receive_ports
-                        if port.endswith("/TCP")
-                    }.isdisjoint(set(asset.open_ports_tcp))
-                ),
-            },
-        ]
-
-        for criteria in matching_criteria:
-            if criteria["test"](asset):
-                confidence += criteria["weight"](asset)
-                evidence.append(
-                    {
-                        "reason": str(criteria["reason"]),
-                        "value": str(criteria["value"](asset)),
-                    }
-                )
-
-        return {
-            "confidence": confidence,
-            "confidence_limit": sum([x["max_weight"] for x in matching_criteria]),
-            "evidence": evidence,
-            "asset": asset,
-        }
-
     def update_suggestions(self):
         """Update EndpointSuggestion models for this NetworkEndpoint."""
         self.clean_fields()
@@ -331,7 +199,8 @@ class NetworkEndpoint(models.Model):
     def clean(self):
         """Ensure model is valid."""
         if not (self.mac_address or self.ipv4_address or self.ipv6_address):
-            raise ValidationError("A MAC or IP address must be provided")
+            msg = "A MAC or IP address must be provided"
+            raise ValidationError(msg)
 
     def merge(self, other):
         """Merge with another NetworkEndpoint.
@@ -364,18 +233,3 @@ class NetworkEndpoint(models.Model):
         self.add_transmit_ports(other.transmit_ports)
         self.add_receive_ports(other.receive_ports)
         return True
-
-    def __hash__(self):
-        """Hash this model."""
-        # combination of Model type and PK is unique
-        return hash((self.__class__, self.pk))
-
-    def __eq__(self, other):
-        """Equal if both NetworkEndpoint and primary key is equal."""
-        return isinstance(other, self.__class__) and self.pk == other.pk
-
-    def __str__(self):
-        return (
-            f"Netflow endpoint with MAC {self.mac_address} "
-            f"and IP {(self.ipv4_address, self.ipv6_address)}"
-        )
