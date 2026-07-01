6077dd0802c979dfd0336cdb67ce6e813bc9bef5
Taylor Cochran <tcochran.dev@gmail.com>
2026-07-01 12:30:41 -0700

    Re-align create_assets and sample assets

--- blueflow/management/commands/create_assets.py
+++ blueflow/management/commands/create_assets.py
@@ -1,39 +1,43 @@
 """Load assets from a json file."""
 
 import json
-from collections.abc import Generator
-from pathlib import Path
+import pathlib
+import typing
+from collections import abc
 
 from django.apps import apps
-from django.core.management.base import BaseCommand
+from django.core.management import base
 
 
-def make_assets(data: dict) -> Generator[dict, None, None]:
-    for _id, asset in data.items():
+def _get_field(asset: dict, key: str, parent: str = "fields") -> typing.Any:
+    return asset.get(parent, {})[key]
+
+
+def make_assets(data: list[dict]) -> abc.Generator[dict, None, None]:
+    for asset in data:
         yield {
-            "id": _id,
-            "name": asset["name"],
-            "hostname": asset["hostname"],
-            "ip_address": asset["ip_address"],
-            "mac_address": asset["mac_address"],
-            "oui_manufacturer": asset["nic_vendor"],
-            "manufacturer": asset["manufacturer"],
-            "model": asset["product"],
-            "serial_number": asset["serial_number"],
-            "udi": asset["udi"],
-            "tag_number": asset["tag_number"],
-            "category": asset["category"],
-            "owner": asset["owner"],
-            "os": asset["os"],
-            "app_sw_version": asset["app_sw_version"],
-            "last_scanned": asset["last_scanned"],
-            "last_pinged": asset["last_pinged"],
-            "open_ports_tcp": asset["open_ports_tcp"],
-            "external_keys": asset["external_keys"],
+            "id": asset["pk"],
+            "name": _get_field(asset, "name"),
+            "hostname": _get_field(asset, "hostname"),
+            "ip_address": _get_field(asset, "ip_address"),
+            "mac_address": _get_field(asset, "mac_address"),
+            "oui_manufacturer": _get_field(asset, "nic_vendor"),
+            "manufacturer": _get_field(asset, "manufacturer"),
+            "model": _get_field(asset, "model"),
+            "serial_number": _get_field(asset, "serial_number"),
+            "udi": _get_field(asset, "udi"),
+            "tag_number": _get_field(asset, "tag_number"),
+            "category": _get_field(asset, "category"),
+            "owner": _get_field(asset, "owner"),
+            "os": _get_field(asset, "os"),
+            "app_sw_version": _get_field(asset, "app_sw_version"),
+            "last_scanned": _get_field(asset, "last_scanned"),
+            "last_pinged": _get_field(asset, "last_pinged"),
+            "external_keys": _get_field(asset, "external_keys"),
         }
 
 
-class Command(BaseCommand):
+class Command(base.BaseCommand):
     """Django manage.py sub command loads assets from a json file."""
 
     help = "Load assets from a json file"
@@ -46,8 +50,8 @@ class Command(BaseCommand):
     def handle(self, *_, **options) -> None:
         Asset = apps.get_model("blueflow", "Asset")
         file_path = options.get("filepath")
-        with Path(file_path).open(encoding="utf-8") as file:
-            data = json.loads(file.read())
+        with pathlib.Path(file_path).open(encoding="utf-8") as file:
+            data = json.load(file)
             assets = make_assets(data)
             Asset.objects.bulk_create([Asset(**asset) for asset in assets])
         self.stdout.write(
