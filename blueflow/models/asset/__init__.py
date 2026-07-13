458b48f7dcc727b253f8d0c6e56c9672858739f1
Taylor Cochran <tcochran.dev@gmail.com>
2026-07-13 08:49:46 -0700

    Regenerate blueflow migrations
    Adding a parent to an existing child creates an unusually painful issue.
    Specifically the child will want a non-null FK to the parent. This
    is how Django creates the relationship between the two. HOWEVER
    the migration will notice that Child already exists and will request
    a default value from the developer. This is an error, any value is
    a false value. The Parent table has no entries thus no default
    would be a correct value. The simpliest approach is to detroy
    and regenerate migrations. An option that is only avaliable
    to Blueflow do to it's unique nature of having no constnat production
    environment.

--- blueflow/models/asset/__init__.py
+++ blueflow/models/asset/__init__.py
@@ -1,11 +1,12 @@
 from .asset import Asset
 from .external_system import ExternalSystem
 from .system import System
-from .util import validate_tcp_port_range
+from .util import default_timestamp, validate_tcp_port_range
 
 __all__ = [
     "Asset",
     "ExternalSystem",
     "System",
+    "default_timestamp",
     "validate_tcp_port_range",
 ]
