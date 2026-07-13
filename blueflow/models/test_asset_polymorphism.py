12d7c4e384379044163a183f3f0b60d1b5b0f301
Taylor Cochran <tcochran.dev@gmail.com>
2026-07-13 09:03:43 -0700

    Confirm polymorphic tests work

--- blueflow/models/test_asset_polymorphism.py
+++ blueflow/models/test_asset_polymorphism.py
@@ -1,19 +1,27 @@
+import pytest
+from model_bakery import baker
+
 from blueflow import models
 
 
-def test_asset_is_system(db) -> None:
+@pytest.fixture
+def create_asset() -> None:
+    _ = baker.make(models.Asset)
+
+
+def test_asset_is_system(create_asset) -> None:
     asset = models.Asset.objects.first()
     assert issubclass(asset.__class__, models.System)
 
 
-def test_asset_is_queryable_from_system(db) -> None:
+def test_asset_is_queryable_from_system(create_asset) -> None:
     system = models.System.objects.first()
     assert hasattr(system, "asset")
 
 
-def test_asset_has_attributes_after_query(db) -> None:
+def test_asset_has_attributes_after_query(create_asset) -> None:
     system = models.System.objects.first()
     assert hasattr(system, "asset")
     asset = system.asset
     assert hasattr(asset, "name")
-    assert hasattr(asset, "ip")
+    assert hasattr(asset, "ip_address")
