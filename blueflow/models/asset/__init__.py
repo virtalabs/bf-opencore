274a92f4dff8d70ef9027c4b749fc290f6ba0aba
Taylor Cochran <tcochran.dev@gmail.com>
2026-07-13 09:08:55 -0700

    Remove historic func

--- blueflow/models/asset/__init__.py
+++ blueflow/models/asset/__init__.py
@@ -1,12 +1,11 @@
 from .asset import Asset
 from .external_system import ExternalSystem
 from .system import System
-from .util import default_timestamp, validate_tcp_port_range
+from .util import default_timestamp
 
 __all__ = [
     "Asset",
     "ExternalSystem",
     "System",
     "default_timestamp",
-    "validate_tcp_port_range",
 ]
