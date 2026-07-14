4af436261ffa8eb942d00b0fdd88ccda72430fd3
Taylor Cochran <tcochran.dev@gmail.com>
2026-07-14 08:46:50 -0700

    Fix grammar/spelling

--- blueflow/models/asset/system.py
+++ blueflow/models/asset/system.py
@@ -4,6 +4,6 @@ from django_extensions.db import models as django_extensions
 class System(django_extensions.TimeStampedModel):
     """System is the parent to all assets and asset like things.
 
-    It's primary role it to provide a centralized query for both
+    Its primary role is to provide a centralized query for both
     internal and external assets and asset like things.
     """
