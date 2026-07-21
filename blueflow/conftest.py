02134939171cb30e808ff505626ac3ce5ad38e48
Taylor Cochran <tcochran.dev@gmail.com>
2026-07-21 12:28:12 -0700

    Fix blueflow/celery/tests/test_viper.py

--- blueflow/conftest.py
+++ blueflow/conftest.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import django.core.management
 import pytest
 
@@ -23,3 +25,13 @@ def setup_assets(django_db_setup, django_db_blocker):
         django.core.management.call_command(
             "create_assets", "--filepath", "data/assets.json"
         )
+
+
+@pytest.fixture
+def mock_post():
+    with patch("blueflow.celery.tasks.requests.post") as mock_post:
+        return_value = mock_post.return_value
+        return_value.status_code = 200
+        return_value.json = dict
+        return_value.raise_for_status.return_value = None
+        yield mock_post
