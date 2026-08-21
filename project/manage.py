8fce282de431f707ba9f37f1b74551f08c9053a8
Taylor Cochran <tcochran.dev@gmail.com>
2026-08-21 10:10:34 -0700

    Fix some linter errors

--- project/manage.py
+++ project/manage.py
@@ -9,12 +9,11 @@ import sys
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.development")
     try:
-        from django.core.management import execute_from_command_line
+        from django.core.management import execute_from_command_line  # noqa: PLC0415
     except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable?",
-        ) from exc
+        msg =  ("Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable?")
+        raise ImportError(msg) from exc
     execute_from_command_line(sys.argv)
 
 
